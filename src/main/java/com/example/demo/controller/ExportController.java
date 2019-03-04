8ce07767af5936ac2e83c400ccc31c09f82565f5
Carole A <carole.astier@free.fr>
2019-03-04 13:15:16 +0100

    TP Etats Imprimés

--- src/main/java/com/example/demo/controller/ExportController.java
+++ src/main/java/com/example/demo/controller/ExportController.java
@@ -1,6 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Client;
+import com.example.demo.entity.Facture;
+import com.example.demo.entity.LigneFacture;
 import com.example.demo.service.ClientService;
+import com.example.demo.service.FactureService;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +19,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Controlleur pour réaliser les exports.
@@ -21,14 +34,157 @@ public class ExportController {
     @Autowired
     private ClientService clientService;
 
+    @Autowired
+    private FactureService factureService;
+
     @GetMapping("/clients/csv")
     public void clientsCSV(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/csv");
         response.setHeader("Content-Disposition", "attachment; filename=\"clients.csv\"");
         PrintWriter writer = response.getWriter();
-        // TODO
-        writer.println("Case00;Case01");
-        writer.println("Case10;Case11");
+
+        List<Client> clients = clientService.findAllClients();
+
+        writer.println("Id;Nom;Prénom;Date de naissance;Age");
+        LocalDate now2 = LocalDate.now();
+
+        for (Client client : clients) {
+
+            writer.println(
+                    client.getId() + ";"
+                            + client.getNom() + ";"
+                            + client.getPrenom() + ";"
+                            + client.getDateNaissance().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ";"
+                            + (now2.getYear() - client.getDateNaissance().getYear())
+            );
+        }
     }
 
+    @GetMapping("/clients/xlsx")
+    public void clientsXLSX(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment; filename=\"clients.xlsx\"");
+        List<Client> clients = clientService.findAllClients();
+        LocalDate now = LocalDate.now();
+
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Clients");
+        Row headerRow = sheet.createRow(0);
+
+
+        Cell cellHeaderId = headerRow.createCell(0);
+        cellHeaderId.setCellValue("Id");
+
+        Cell cellHeaderPrenom = headerRow.createCell(1);
+        cellHeaderPrenom.setCellValue("Prénom");
+
+        Cell cellHeaderNom = headerRow.createCell(2);
+        cellHeaderNom.setCellValue("Nom");
+
+        Cell cellHeaderDateNaissance = headerRow.createCell(3);
+        cellHeaderDateNaissance.setCellValue("Date de naissance");
+
+        Cell cellHeaderAge = headerRow.createCell(4);
+        cellHeaderAge.setCellValue("Age");
+
+        int i = 1;
+        for (Client client : clients) {
+            Row row = sheet.createRow(i);
+
+            Cell cellId = headerRow.createCell(0);
+            cellId.setCellValue(client.getId());
+
+            Cell cellPrenom = row.createCell(1);
+            cellPrenom.setCellValue(client.getPrenom());
+
+            Cell cellNom = row.createCell(2);
+            cellNom.setCellValue(client.getNom());
+
+            Cell cellDateNaissance = row.createCell(3);
+            cellDateNaissance.setCellValue(client.getDateNaissance().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+            Cell cellAge = row.createCell(4);
+            cellAge.setCellValue((now.getYear() - client.getDateNaissance().getYear()));
+
+            i++;
+
+
+        }
+
+        workbook.write(response.getOutputStream());
+        workbook.close();
+
+    }
+
+    @GetMapping("/factures/xlsx")
+    public void facturesXlsx(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment; filename=\"factures.xlsx\"");
+
+        List<Facture> factures = factureService.findAllFactures();
+        Workbook workbook = new XSSFWorkbook();
+
+
+        for (Facture facture : factures) {
+
+            Double prixTotal = 0d;
+
+            Sheet sheet = workbook.createSheet("Factures" + facture.getId());
+
+            Row headerRow = sheet.createRow(0);
+
+
+            Cell cellHeaderArticle = headerRow.createCell(0);
+            cellHeaderArticle.setCellValue("Article");
+
+            Cell cellHeaderQuantite = headerRow.createCell(1);
+            cellHeaderQuantite.setCellValue("Quantité");
+
+            Cell cellHeaderPrixUnitaire = headerRow.createCell(2);
+            cellHeaderPrixUnitaire.setCellValue("Prix Unitaire");
+
+            Cell cellHeaderPrixLigne = headerRow.createCell(3);
+            cellHeaderPrixLigne.setCellValue("Prix Ligne");
+
+            int i = 2;
+
+            for (LigneFacture ligneFacture : facture.getLigneFactures()) {
+                Row row = sheet.createRow(i);
+
+                Cell cellArticle = row.createCell(0);
+              cellArticle.setCellValue(ligneFacture.getArticle().getLibelle());
+
+
+
+                Cell cellQuantite = row.createCell(1);
+                cellQuantite.setCellValue(ligneFacture.getQuantite());
+
+                Cell cellPrixUnitaire = row.createCell(2);
+                cellPrixUnitaire.setCellValue(ligneFacture.getArticle().getPrix());
+
+                Cell cellPrixLigne = row.createCell(3);
+                cellPrixLigne.setCellValue(ligneFacture.getArticle().getPrix() * ligneFacture.getQuantite());
+
+                prixTotal += ligneFacture.getArticle().getPrix() * ligneFacture.getQuantite();
+
+
+
+                i++;
+
+
+
+
+            }
+            Row row = sheet.createRow(i+2);
+            Cell cellPrixTotal = row.createCell (3);
+            cellPrixTotal.setCellValue(prixTotal);
+
+            workbook.write(response.getOutputStream());
+            workbook.close();
+        }
+
+
+
+
+    }
 }
--- src/main/java/com/example/demo/service/InitData.java
+++ src/main/java/com/example/demo/service/InitData.java
@@ -1,13 +1,18 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Article;
 import com.example.demo.entity.Client;
+import com.example.demo.entity.Facture;
+import com.example.demo.entity.LigneFacture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
 
 /**
  * Classe permettant d'insérer des données dans l'application.
@@ -25,17 +30,50 @@ public class InitData implements ApplicationListener<ApplicationReadyEvent> {
     }
 
     private void insertTestData() {
-        Client client1 = newClient("PETRILLO", "Alexandre");
+        Client client1 = newClient("PETRILLO", "Alexandre", LocalDate.of(1983, 12, 19));
         em.persist(client1);
 
-        Client client2 = newClient("Dupont", "Jérome");
+        Client client2 = newClient("Dupont", "Jérome", LocalDate.of(1990,9,9));
         em.persist(client2);
+
+        Client client3 = newClient("Doe", "John", LocalDate.of(1975, 4, 8));
+        em.persist(client3);
+
+        Article a1 = new Article();
+        a1.setLibelle("Balayette");
+        a1.setPrix(3.99);
+        em.persist(a1);
+
+        Article a2 = new Article();
+        a2.setLibelle("Stylo espion");
+        a2.setPrix(130);
+        em.persist(a2);
+
+        Facture f1 = new Facture();
+        f1.setClient(client1);
+        em.persist(f1);
+        em.persist(newLigneFacture(f1, a1, 2));
+        em.persist(newLigneFacture(f1, a2, 1));
+
+        Facture f2 = new Facture();
+        f2.setClient(client2);
+        em.persist(f2);
+        em.persist(newLigneFacture(f2, a1, 10));
     }
 
-    private Client newClient(String nom, String prenom) {
+    private LigneFacture newLigneFacture(Facture f, Article a1, int quantite) {
+        LigneFacture lf1 = new LigneFacture();
+        lf1.setArticle(a1);
+        lf1.setQuantite(quantite);
+        lf1.setFacture(f);
+        return lf1;
+    }
+
+    private Client newClient(String nom, String prenom, LocalDate dateNaissance) {
         Client client = new Client();
         client.setNom(nom);
         client.setPrenom(prenom);
+        client.setDateNaissance(dateNaissance);
         return client;
     }
 }
